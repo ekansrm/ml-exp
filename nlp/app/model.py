b5982e28465df8fad2af480bcb9854df2234c02e
ekansrm <ekansrm0001@163.com>
2018-08-03 20:08:32 +0800

    big model

--- nlp/app/model.py
+++ nlp/app/model.py
@@ -66,29 +66,53 @@ if __name__ == '__main__':
     # 先测试句子分割为多个向量
 
     test_data = []
-    with open("data1.dat", 'r') as fp:
+    with open("data1.dat", 'r', encoding='utf-8') as fp:
         for line in fp.readlines():
+            line = line.strip()
             if '' == line:
                 continue
             test_data.append(line)
-    print(test_data)
-
-    # def slice(x, h1, h2, w1, w2):
-    #     """ Define a tensor slice function
-    #     """
-    #     return x[:, h1:h2, w1:w2, :]
-    #
-    # slice_1 = Lambda(slice, arguments={'h1': 0, 'h2': 6, 'w1': 0, 'w2': 6})(sliced)
 
     # 先手动切割
+    dep_set = set()
 
     def slice(x: str):
+        global dep_set
         items = x.split("/")
-        return items[0::3], items[1::3], items[2::3]
+        dep = items[0::3]
+        op1 = items[1::3]
+        op2 = items[2::3]
+        # tokenizer
+        print(dep)
+        print(op1)
+        print(op2)
+
+        # embedding
+
+        return dep, op1, op2
 
     split_data = [slice(l) for l in test_data]
 
-    print(split_data)
+    # 每个分行符是一个批次的分割
+
+    # 生成标记
+    #   方案1, 全排列
+    #   方案2, 按照批次大小, 生成一个目标样本数, 然后随机生成行数的两个数, 生成样本
+
+    # 句子距离
+    #   方案1, 二元值, 有多大可能在前面还是后面
+    #   方案2, 距离度量
+
+    # embedding 方案
+    #   先padding, 在运行时转换
+    #   先转换, 在padding, 在输入模型
+
+    print(dep_set)
+
+    # 手动padding
+
+
+    # print(split_data)
 
 
 
